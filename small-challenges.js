08f938661c596f8dd33153a0c6b946379587d634
Adam Hemming <hemming.dev@gmail.com>
2022-09-17 23:42:48 +0900

    create factorial function

--- small-challenges.js
+++ small-challenges.js
@@ -7,3 +7,8 @@ function power(base, exponent) {
   return exponent === 1 ? base : base * power(base, exponent - 1);
 }
 console.log(power(2, 4));
+
+function factorial(num) {
+  return num === 1 ? num : num * factorial(num - 1);
+};
+console.log(factorial(5));
