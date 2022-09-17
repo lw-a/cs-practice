3f709afb8af1833a2585926dc5316dc3169f2598
Adam Hemming <hemming.dev@gmail.com>
2022-09-17 23:40:47 +0900

    create power function

--- small-challenges.js
+++ small-challenges.js
@@ -1,4 +1,9 @@
 function sumRange(num) {
-  return num === 1 ? num : num + sumRange(num - 1);
-  }
-  console.log(sumRange(3));
+return num === 1 ? num : num + sumRange(num - 1);
+}
+console.log(sumRange(3));
+
+function power(base, exponent) {
+  return exponent === 1 ? base : base * power(base, exponent - 1);
+}
+console.log(power(2, 4));
