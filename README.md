606591c6c65322aeae1978f62331af9b57f3d5b4
Adam Hemming <hemming.dev@gmail.com>
2022-09-18 00:01:07 +0900

    create function to check it every element in array satisfies some condition

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # recursion
+
+Learning recursion
--- small-challenges.js
+++ small-challenges.js
@@ -1,14 +1,37 @@
 function sumRange(num) {
 return num === 1 ? num : num + sumRange(num - 1);
 }
-console.log(sumRange(3));
+console.log(sumRange(3)); // 6
 
 function power(base, exponent) {
   return exponent === 1 ? base : base * power(base, exponent - 1);
 }
-console.log(power(2, 4));
+console.log(power(2, 4)); // 16
 
 function factorial(num) {
   return num === 1 ? num : num * factorial(num - 1);
 };
-console.log(factorial(5));
+console.log(factorial(5)); // 120
+
+function all(array, callback) {
+  if (array.length === 0) {
+    return true;
+  };
+
+  if (callback(array[0])) {
+    array.shift();
+    return all(array, callback);
+
+  } else {
+    return false;
+  };
+};
+const allAreLessThanSeven = all([1,2,9], function(num){
+	return num < 7;
+});
+console.log(allAreLessThanSeven); // false
+
+const allAreLessThanNine = all([1,2,6,5,7,8], function(num){
+	return num < 9;
+});
+console.log(allAreLessThanNine); // true
