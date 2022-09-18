4658cb8377b02154dd029b72a0eadafca572f364
Adam Hemming <hemming.dev@gmail.com>
2022-09-19 02:34:19 +0900

    bed time

--- merge_sort.js
+++ merge_sort.js
@@ -1,3 +1,16 @@
 function mergeSort(array) {
+if (array.length < 2) return array;
 
+const middle = array.length / 2;
+
+// sort left half
+const left = array.slice(0, middle);
+// if (left.length)
+mergeSort(left);
+
+// sort right half
+const right = array.slice(middle, array.length);
+mergeSort(right);
+
+// merge sorted halves
 };
