1f2dfd81467f4094e7d32a3cf8891f74206f5a59
Adam Hemming <hemming.dev@gmail.com>
2022-09-18 20:47:36 +0900

    make function to return array of given number x times

--- challenges.js
+++ challenges.js
@@ -129,3 +129,12 @@ console.log(sumSquares(l)); // 1 = 1
 
 l = [10,[[10],10],[10]]
 console.log(sumSquares(l)); // 100 + 100 + 100 + 100 = 400
+
+// Replicate
+function replicate(times, number) {
+  if (times <= 0) return [];
+  return [number].concat(replicate(times - 1, number));
+}
+console.log(replicate(3, 5)) // [5, 5, 5]
+console.log(replicate(1, 69)) // [69]
+console.log(replicate(-2, 6)) // []
