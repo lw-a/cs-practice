7e75246776c7da2d2844c1de0662f5c0c80dd532
Adam Hemming <hemming.dev@gmail.com>
2022-09-20 02:13:23 +0900

    create skeleton

---
+++ data_structures/linked_lists.js
@@ -0,0 +1,15 @@
+// Node
+const Node = (value) => {
+
+  return { value, nextNode: null };
+};
+
+const test = Node(19);
+console.log(test.value)
+console.log(test.nextNode)
+
+// Linked List
+const LinkedList = () => {
+
+  return { };
+};
