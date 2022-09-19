9fc6107cb9593e23a77d00f076db7a58659528f3
Adam Hemming <hemming.dev@gmail.com>
2022-09-19 20:09:25 +0900

    finally figure out how to make merge sort

--- merge_sort.js
+++ merge_sort.js
@@ -1,16 +1,27 @@
 function mergeSort(array) {
-if (array.length < 2) return array;
+if (array.length <= 1) return array;
 
 const middle = array.length / 2;
 
-// sort left half
-const left = array.slice(0, middle);
-// if (left.length)
-mergeSort(left);
+let left = array.slice(0, middle);
+left = mergeSort(left);
 
-// sort right half
-const right = array.slice(middle, array.length);
-mergeSort(right);
+let right = array.slice(middle, array.length);
+right = mergeSort(right);
 
-// merge sorted halves
+return merge(left, right);
 };
+
+function merge(left, right) {
+
+  const merged = [];
+
+  while (left.length > 0 && right.length > 0) {
+    const smallerArray = left[0] < right[0] ? left : right;
+    merged.push(smallerArray.shift());
+  };
+
+  return merged.concat(left, right);
+};
+
+console.log(mergeSort([5, 2, 11, 8, -1, 4, 12, 0, 22, 7])); // [ -1, 0,  2,  4,  5, 7, 8, 11, 12, 22 ]
