5b4fc749c78d3548831358e829d5caf2ae05b7e8
Adam Hemming <hemming.dev@gmail.com>
2022-09-20 02:40:54 +0900

    create append function

--- data_structures/linked_lists.js
+++ data_structures/linked_lists.js
@@ -1,9 +1,10 @@
 // Node
-const Node = (value) => {
+const Node = (value = null) => {
 
   return { value, nextNode: null };
 };
 
+console.log(Node())
 const test = Node(19);
 console.log(test.value)
 console.log(test.nextNode)
@@ -11,5 +12,29 @@ console.log(test.nextNode)
 // Linked List
 const LinkedList = () => {
 
-  return { };
+  let head = null;
+
+  // append(value)
+  const append = (value) => {
+    const node = Node(value);
+
+    if (head === null) return head = node;
+
+    let pointer = head;
+    while (pointer.nextNode != null) {
+      pointer = pointer.nextNode;
+    };
+
+    pointer.nextNode = node;
+  };
+
+  return { append };
 };
+
+const list = LinkedList();
+console.log(list)
+list.append(1)
+list.append(2)
+list.append(3)
+console.log(list.append(4))
+// how do I test if it works?
