0d6a2b5e8521262096bd92740508484e46bdc419
Adam Hemming <hemming.dev@gmail.com>
2022-09-20 15:42:27 +0900

    create prepend function

--- data_structures/linked_lists.js
+++ data_structures/linked_lists.js
@@ -28,7 +28,19 @@ const LinkedList = () => {
     pointer.nextNode = node;
   };
 
-  return { append };
+    // prepend(value)
+    const prepend = (value) => {
+      const node = Node(value);
+
+      if (head === null) return head = node;
+
+      let temp = head.nextNode;
+
+      head = node;
+      head.nextNode = temp;
+    };
+
+  return { append, prepend };
 };
 
 const list = LinkedList();
@@ -38,3 +50,4 @@ list.append(2)
 list.append(3)
 console.log(list.append(4))
 // how do I test if it works?
+list.prepend(0)
