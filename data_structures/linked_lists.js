ef17530a5ef41657eea3a12102a47fc562b5df5a
Adam Hemming <hemming.dev@gmail.com>
2022-09-21 18:38:33 +0900

    fix big when trying to run functions when list has 0 nodes

--- data_structures/linked_lists.js
+++ data_structures/linked_lists.js
@@ -3,11 +3,6 @@ const Node = (value = null) => {
   return { value, nextNode: null };
 };
 
-// console.log(Node())
-// const test = Node(19);
-// console.log(test.value)
-// console.log(test.nextNode)
-
 // Linked List
 const LinkedList = () => {
 
@@ -26,93 +21,105 @@ const LinkedList = () => {
     pointer.nextNode = node;
   };
 
-    // prepend(value)
-    const prepend = (value) => {
-      const node = Node(value);
-      length ++;
-      if (HEAD === null) return HEAD = node;
-      let temp = HEAD.nextNode;
-      HEAD = node;
-      HEAD.nextNode = temp;
+  // prepend(value)
+  const prepend = (value) => {
+    const node = Node(value);
+    length ++;
+    if (HEAD === null) return HEAD = node;
+    let temp = HEAD.nextNode;
+    HEAD = node;
+    HEAD.nextNode = temp;
+  };
+
+  // size
+  const size = () => {
+    return length;
+  };
+
+  // head
+  const head = () => {
+    if (!HEAD) return null;
+
+    return HEAD;
+  };
+
+  // tail
+  const tail = () => {
+    if (!HEAD) return null;
+
+    let pointer = HEAD;
+    while (pointer.nextNode !== null) {
+      pointer = pointer.nextNode;
+    };
+    return pointer;
+  };
+
+  // at(index)
+  const at = (index) => {
+    if (!HEAD) return null;
+
+    if (index > length) return null;
+    if (index === 0) return HEAD;
+    let pointer = HEAD;
+    for (let i = 1; i < index; i++) {
+      pointer = pointer.nextNode;
+    };
+    return pointer;
+  };
+
+  // pop
+  const pop = () => {
+    if (!HEAD) return null;
+
+    const popped = tail();
+
+    at(length - 2).nextNode = null;
+    length--;
+
+    return popped;
+  };
+
+  // contains(value)
+  const contains = (value) => {
+    if (!HEAD) return null;
+
+    let pointer = HEAD;
+    while (pointer.nextNode !== null) {
+      if (pointer.value === value) return true;
+      pointer = pointer.nextNode;
+    };
+    return tail().value === value ? true : false;
+  };
+
+  // find(value)
+  const find = (value) => {
+    if (!HEAD) return null;
+
+    let pointer = HEAD;
+    let index = 1;
+    if (pointer.value === value) return 0;
+    while (pointer.nextNode !== null) {
+      if (pointer.value === value) return index;
+      pointer = pointer.nextNode;
+      index++;
+    };
+    return tail().value === value ? index : null;
+  };
+
+  // toString
+  const toString = () => {
+    if (!HEAD) return null;
+
+    let string = "";
+    let pointer = HEAD;
+
+    while (pointer.nextNode !== null) {
+      string += `(${pointer.value}) -> `;
+      pointer = pointer.nextNode;
     };
 
-    // size
-    const size = () => {
-      return length;
-    };
-
-    // head
-    const head = () => {
-      return HEAD;
-    };
-
-    // tail
-    const tail = () => {
-      let pointer = HEAD;
-      while (pointer.nextNode !== null) {
-        pointer = pointer.nextNode;
-      };
-      return pointer;
-    };
-
-    // at(index)
-    const at = (index) => {
-      if (index > length) return null;
-      if (index === 0) return HEAD;
-      let pointer = HEAD;
-      for (let i = 1; i < index; i++) {
-        pointer = pointer.nextNode;
-      };
-      return pointer;
-    };
-
-    // pop
-    const pop = () => {
-      let pointer = HEAD;
-      const popped = tail();
-
-      at(length - 2).nextNode = null;
-      length--;
-
-      return popped;
-    };
-
-    // contains(value)
-    const contains = (value) => {
-      let pointer = HEAD;
-      while (pointer.nextNode !== null) {
-        if (pointer.value === value) return true;
-        pointer = pointer.nextNode;
-      };
-      return tail().value === value ? true : false;
-    };
-
-    // find(value)
-    const find = (value) => {
-      let pointer = HEAD;
-      let index = 1;
-      if (pointer.value === value) return 0;
-      while (pointer.nextNode !== null) {
-        if (pointer.value === value) return index;
-        pointer = pointer.nextNode;
-        index++;
-      };
-      return tail().value === value ? index : null;
-    };
-
-    // toString
-    const toString = () => {
-
-      let string = "";
-      let pointer = HEAD;
-
-      while (pointer.nextNode !== null) {
-        string += `(${pointer.value}) -> `;
-        pointer = pointer.nextNode;
-      };
-
-      return string += `(${tail().value}) -> null`
-    };
+    return string += `(${tail().value}) -> null`
+  };
 
   return { append, prepend, size, head, tail, at, pop, contains, find, toString };
 };
