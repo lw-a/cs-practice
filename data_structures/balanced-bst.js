c3faf4c0a90e34c63f308dd94a311044dc7d8c82
Adam Hemming <hemming.dev@gmail.com>
2022-09-21 19:59:49 +0900

    create tree with root

--- data_structures/balanced-bst.js
+++ data_structures/balanced-bst.js
@@ -14,5 +14,9 @@ const Tree = (array) => {
     return root;
   };
 
-  return { buildTree };
+  let root = buildTree(array, 0, array.length - 1)
+
+  return { root };
 };
+
+const tree = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
